85af7a0bc24b78a7ee523f9647595eca6d2c4b0e
ru7an <ru7an.1998@gmail.com>
2021-03-15 12:07:34 +0545

    first

--- Main/home/models.py
+++ Main/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class registration(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    birthday = birthday.fields.BirthdayField()
+    gender = models.CharField(max_length=100)
