04c7502197c67d75a5115db577713beb14c6a350
rusanvaidya <ru7an.1998@gmail.com>
2021-04-07 06:38:58 +0545

    mobile responsive update

--- Main/Main/settings.py
+++ Main/Main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd1)dkh^3+r5@ykk$54k)3ohtv_$r-l0xitf14_u-8*z^q#s&os'
+SECRET_KEY = os.getenv('key');
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
