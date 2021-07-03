c157b6cec0ff7f0324cd8d9325c021b0f0def891
Samir Maharjan <samir84753@gmail.com>
2021-07-03 09:52:28 +0545

    added asgi.py

--- Main/Main/asgi.py
+++ Main/Main/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chatbox.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Main.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chatbox.routing.websocket_urlpatterns
+        )
+    ),
+})
