5b8e2ce9c73a0ed8745986d17069e936b3818c23
Samir Maharjan <samir84753@gmail.com>
2021-07-12 09:22:01 +0545

    added user_coordinates model

--- Main/complete/models.py
+++ Main/complete/models.py
@@ -6,3 +6,8 @@ class userdetails(models.Model):
     profile_pic = models.ImageField(upload_to='user_profile',blank=True)
     user_bio = models.TextField(max_length=1000,blank=True)
     user_interest = models.CharField(max_length=10000,blank=True)
+
+class user_coordinates(models.Model):
+    user=models.ForeignKey(registration,on_delete=models.CASCADE)
+    latitude=models.FloatField(max_length=50,blank=True)
+    longitude=models.FloatField(max_length=50,blank=True)
