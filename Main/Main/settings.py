0d30bc6deed164a14677950bd3cefefc6d831f9c
pradip pandit <pradippandit026@gmail.com>
2021-07-14 12:16:39 +0545

    setting

--- Main/Main/settings.py
+++ Main/Main/settings.py
@@ -155,3 +155,11 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_HOST_USER='spotsterinc@gmail.com'
+EMAIL_HOST_PASSWORD='ulghnucrmhtgnnph'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Spotster Team <noreply@spotster.com>'
